bc117be2e0efd03a51e04fcedb77b39a2eed7902
Nishka Dasgupta <nishka.dasgupta@proton.me>
2023-03-16 14:41:15 +0100

    Rename Sendrier conversion file
    Signed-off-by: Nishka Dasgupta <nishka.dasgupta@proton.me>

--- sendrier.py
+++ sendrier.py
@@ -91,8 +91,9 @@ def encode_fd(delta, d):
     
 def decode_fd(d, B, start):
     u = ceil(log2(d))
+    print('d', d, 'u', u)
     delta = read_bits(B, u-1, start)
-    start = start + u-1
+    start = start + u - 1
     limit = (2 ** u) - d
     if delta >= limit:
         delta = (2 * delta) + read_bits(B, 1, start) - (2 ** u) + d
@@ -111,7 +112,7 @@ def CWtoB(n, t, delta_tuple):
         new_delta_tuple = tuple(new_delta_lst)
         #print('new_delta_tuple', new_delta_tuple)
         res = '1' + CWtoB(n - d, t, new_delta_tuple)
-        #print('res', res)
+        #print('n', n, 't', t, 'd', d, 'res', res)
         return res
     else:
         enc = encode_fd(delta_1, d)
@@ -124,7 +125,7 @@ def CWtoB(n, t, delta_tuple):
         new_delta_tuple = tuple(new_delta_lst)
         #print('new_delta_tuple', new_delta_tuple)
         res = s + CWtoB(n - delta_1 - 1, t - 1, new_delta_tuple)
-        #print('res', res)
+        #print('n', n, 't', t, 'd', d, 'res', res, 'delta_1', delta_1, n-delta_1 - 1)
         return res
         
 def BtoCW(n, t, delta, B, start):
@@ -133,7 +134,7 @@ def BtoCW(n, t, delta, B, start):
         return []
     elif n <= t:
         res = [delta] + BtoCW(n - 1, t - 1, 0, B, start)
-        #print('res', res)
+        print('n', n, 't', t, 'delta', delta, 'B', B, 'res', res)
         return res
     else:
         d = best_d(n, t)
@@ -142,13 +143,14 @@ def BtoCW(n, t, delta, B, start):
         start = start + 1
         if next_bit == 1:
             res = BtoCW(n - d, t, delta + d, B, start)
-            #print('res', res)
+            print('n', n, 't', t, 'd', d, 'delta', delta, 'B', B, 'res', res)
             return res
         else:
             i, start = decode_fd(d, B, start)
+            #i, start = decode_fd(d, B, 0)
             #print('i', i, 'd', d)
             res = [delta + i] + BtoCW(n - i - 1, t - 1, 0, B, start)
-            #print('res', res)
+            print('n', n, 't', t, 'd', d, 'i', i, 'start', start, 'delta', delta, 'B', B, 'res', res)
             return res
 
 def test_decode_encode_fd():
@@ -275,30 +277,33 @@ def test_reverse_bijection():
     n = 30
     t = 5
     nct = int(ceil(factorial(n)/(factorial(t) * factorial(n-t))))
-    bitlength = int(ceil(log2(nct)))
-    for i in range(10):
+    bitlength = int(ceil(log2(nct))) - 1
+    for i in range(1):
         r = random_matrix(Integers(2), 1, bitlength)
         B = ''
-        for ele in r[0]:
-            B = B + str(ele)
+        #for ele in r[0]:
+        #    B = B + str(ele)
         #print(len(B))
-        B = int(B, 2) % nct
-        B = bin(B)[2:]
-        while len(B) < bitlength:
-            B = '0' + B
+        #B = int(B, 2) % nct
+        #B = bin(B)[2:]
+        #while len(B) < bitlength:
+        #    B = '0' + B
         #print(bitlength)
+        #B = '00100010000011010'
+        B = '00010011111100001'
         delta_lst = BtoCW(n, t, 0, B, 0)
         res = CWtoB(n, t, tuple(delta_lst))
         if not (res == B):
+            print(delta_lst)
             print(res, B)
         
-
+'''
 test_decode_encode_fd()
 print("decode_fd(encode_fd()) works")
 test_encode_decode_fd()
 print("encode_fd(decode_fd()) works")
 
-'''
+
 test_BtoCW_weight()
 print("BtoCW returns values of correct weight")
 test_BtoCW_unique()
@@ -308,6 +313,5 @@ print("CWtoB always returns the same output on an input")
 
 test_conversion_bijective()
 print("BtoCW(CWtoB()) works")
-
-test_reverse_bijection()
 '''
+test_reverse_bijection()
